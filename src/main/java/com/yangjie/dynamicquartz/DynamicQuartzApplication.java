fc837885e2dd1e5d7d8e88a90c840ffed1d571bf
Anwersbubu <1055658601@qq.com>
2021-10-27 20:24:16 +0800

    初始化项目

---
+++ src/main/java/com/yangjie/dynamicquartz/DynamicQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.yangjie.dynamicquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicQuartzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangjie/dynamicquartz/DynamicQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangjie.dynamicquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
